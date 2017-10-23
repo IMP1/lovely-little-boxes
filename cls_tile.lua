47fbf90be31c074038d34da2da5c693bcdb86c6d
Huw Taylor <huw_taylor@hotmail.co.uk>
2017-10-23 16:05:18 +0100

    added passablity of tiles which is dependant on certain factors

--- cls_tile.lua
+++ cls_tile.lua
@@ -1,5 +1,29 @@
 local TILE_SIZE = 64
 
-return {
-    tileSize = TILE_SIZE
+local tiles = {
+    GROUND = 1, -- default
+    GOAL   = 2, -- where the player needs to get
+    WATER  = 3, -- player cannot pass unless in box
+    ICE    = 4, -- player (and any boxes) keep moving until they cannot
+    TUNNEL = 5, -- player can only pass if not in a box
 }
+
+function tiles.isPassable(tile, isInBox)
+    if tile <= 0 then
+        return false
+    end
+    if tile == tiles.GROUND or tile == tiles.GOAL or tile == tiles.ICE then
+        return true
+    end
+    if tile == tiles.WATER then
+        return isInBox
+    end
+    if tile == tiles.TUNNEL then
+        return not isInBox
+    end
+    return false
+end
+
+tiles.tileSize = TILE_SIZE
+
+return tiles
